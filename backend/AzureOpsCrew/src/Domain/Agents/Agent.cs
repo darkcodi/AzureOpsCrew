d7337555c83a08122dfcee23c2fde519e363a76b
karzhole <260738057+karzhole@users.noreply.github.com>
2026-02-12 15:31:33 +0100

    Add Agent aggregate root

---
+++ backend/AzureOpsCrew/src/Domain/Agents/Agent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureOpsCrew.Domain.Agents
+{
+    public class Agent(Guid guid, int clientId, AgentInfo info, Provider provider, string providerAgentId)
+    {
+        public Guid Guid { get; private set; } = guid;
+
+        public string ProviderAgentId { get; private set; } = providerAgentId;
+
+        public int ClientId { get; private set; } = clientId;
+        
+        public AgentInfo Info { get; private set; } = info;
+
+        public Provider Provider { get; private set; } = provider;
+
+        public AgentTool[] AvaliableTools { get; set; } = [];
+
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+    }
+}
---
+++ backend/AzureOpsCrew/src/Domain/Agents/Tool.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureOpsCrew.Domain.Agents
+{
+    public enum AgentTool
+    {
+        AzureMcp = 0
+    }
+}
