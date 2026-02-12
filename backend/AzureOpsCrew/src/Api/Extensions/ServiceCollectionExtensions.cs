c12e3f68d807db4e5daea2970e3bc1c93ab59981
darkcodi <trooper982@gmail.com>
2026-02-12 01:41:57 -0600

    Disable cosmos ssl validation for local env

--- backend/AzureOpsCrew/src/Api/Extensions/ServiceCollectionExtensions.cs
+++ backend/AzureOpsCrew/src/Api/Extensions/ServiceCollectionExtensions.cs
@@ -7,11 +7,31 @@ namespace AzureOpsCrew.Api.Extensions
     {
         public static void AddEFCoreCosmosDb(this IServiceCollection services, IConfiguration configuration, string configurationKey)
         {
+            var disableSslValidation = configuration.GetValue<bool>($"{configurationKey}:DisableSslValidation");
+            var connectionMode = configuration.GetValue<string>($"{configurationKey}:ConnectionMode");
+
             services.AddDbContext<AzureOpsCrewContext>(options =>
                 options.UseCosmos(
                     configuration[$"{configurationKey}:AccountEndpoint"]!,
                     configuration[$"{configurationKey}:AccountKey"]!,
-                    configuration[$"{configurationKey}:DatabaseName"]!));
+                    configuration[$"{configurationKey}:DatabaseName"]!,
+                    cosmosOptions =>
+                    {
+                        if (disableSslValidation)
+                        {
+                            cosmosOptions.HttpClientFactory(() =>
+                            {
+                                var handler = new HttpClientHandler();
+                                handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+                                return new HttpClient(handler);
+                            });
+                        }
+
+                        if (Enum.TryParse<Microsoft.Azure.Cosmos.ConnectionMode>(connectionMode, true, out var mode))
+                        {
+                            cosmosOptions.ConnectionMode(mode);
+                        }
+                    }));
         }
     }
 }
