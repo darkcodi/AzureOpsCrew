49ad523b7213d1555686b170c75a66776d4d8745
karzhole <260738057+karzhole@users.noreply.github.com>
2026-02-18 15:33:45 +0100

    Refactor Seeder

---
+++ backend/src/Api/Setup/Seeds/SeederOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace AzureOpsCrew.Api.Setup.Seeds
+{
+    public record SeederOptions
+    {
+        public bool IsEnabled { get; set; }
+
+        public ProviderSeedData AzureFoundrySeed { get; set;}
+    }
+
+    public record ProviderSeedData
+    {
+        public string ApiEndpoint { get; set;}
+
+        public string Key { get; set; }
+    }
+}
