969bcde787d0ea2594e2e47314030b924772f671
darkcodi <trooper982@gmail.com>
2026-03-01 09:34:41 -0600

    Change UserId/AgentId from string to Guid in DirectMessageChannel

--- backend/src/Domain/Chats/DirectMessageChannel.cs
+++ backend/src/Domain/Chats/DirectMessageChannel.cs
@@ -3,9 +3,9 @@ namespace AzureOpsCrew.Domain.Chats;
 public class DirectMessageChannel
 {
     public Guid Id { get; set; }
-    public string? User1Id { get; set; }
-    public string? User2Id { get; set; }
-    public string? Agent1Id { get; set; }
-    public string? Agent2Id { get; set; }
+    public Guid? User1Id { get; set; }
+    public Guid? User2Id { get; set; }
+    public Guid? Agent1Id { get; set; }
+    public Guid? Agent2Id { get; set; }
     public DateTime CreatedAt { get; set; }
 }
