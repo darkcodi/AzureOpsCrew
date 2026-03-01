e4c26e479f5c067210798e5951ffbd6b558e373a
darkcodi <trooper982@gmail.com>
2026-03-01 16:39:54 -0600

    Remove Worker project

--- backend/src/Worker/Settings/TemporalSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace AzureOpsCrew.Worker.Settings;
-
-public sealed class TemporalSettings
-{
-    public string Host { get; set; } = "localhost";
-    public int Port { get; set; } = 7233;
-
-    public string GetTarget() => $"{Host}:{Port}";
-}
